cc3708a6686765dfd3261afb34d009aafe2a12e2
Petenk0 <petenko.p@gmail.com>
2022-07-04 17:11:40 +0200

    nvm

--- js/app.js
+++ js/app.js
@@ -1,122 +1,93 @@
 //DATA
 
-let word = ''
+let word = "";
 
 // | 0 zapis oreže desatinnu časť čisla tj. nastavy ho na integer
 // let solution = 'balls'
-let solution = allWords[allWords.length * Math.random() | 0 ].toLowerCase()
-let tries = 1
-const maxWordLength = 5
-const maxTries = 6
-
+let solution = allWords[(allWords.length * Math.random()) | 0].toLowerCase();
+let tries = 1;
+const maxWordLength = 5;
+const maxTries = 6;
 
 //KEYBOARD
 
-document.addEventListener('keydown',(event) => {
-
-    if (event.key === 'Enter') {
-
-        submitWord()
-    }
-
-    else if (event.key === 'Backspace') {
-
-        removeLetter()
-    }
-
-    else{
-
-        addLetter(event.key)
-    }
-
-
-
-} )
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    submitWord();
+  } else if (event.key === "Backspace") {
+    removeLetter();
+  } else {
+    addLetter(event.key);
+  }
+});
 
 //SUBMIT WORD
 const submitWord = () => {
-    
-    if (word.length !== maxWordLength) return
+  if (word.length !== maxWordLength) return;
 
-    //is this a real word
-    if(!allWords.includes(word)){
-        animateRowShake(currentRow())
-        return
-    }
+  //is this a real word
+  if (!allWords.includes(word)) {
+    animateRowShake(currentRow());
+    return;
+  }
 
-    animateTileReveal(currentRow())
+  animateTileReveal(currentRow());
 
-    setTimeout(() => {
-        judgeResult(word)
-    }, 1500);
-    
-}
-
+  setTimeout(() => {
+    judgeResult(word);
+  }, 1500);
+};
 
 //ADD LETTER
 const addLetter = (character) => {
-    if (word.length >= maxWordLength) return
-    
-    // allow only letters
-    if(/^\p{L}$/u.test(character)){
+  if (word.length >= maxWordLength) return;
 
-        word = word + character
-        word = word.toLowerCase()
+  // allow only letters
+  if (/^\p{L}$/u.test(character)) {
+    word = word + character;
+    word = word.toLowerCase();
 
-        let tile = currentTile()    
-        tile.innerHTML = character
+    let tile = currentTile();
+    tile.innerHTML = character;
 
-        animateTileBounce(tile)
-    }
-      
-}
-
+    animateTileBounce(tile);
+  }
+};
 
 //REMOVE LETTER
 const removeLetter = () => {
-    if (word.length <= 0) return
-    
-    let tile = currentTile()
-    tile.className = 'tile'
+  if (word.length <= 0) return;
 
-    tile.innerHTML = ''
+  let tile = currentTile();
+  tile.className = "tile";
 
-    word = word.slice(0, -1)  
-}
+  tile.innerHTML = "";
+
+  word = word.slice(0, -1);
+};
 
 //TILE TO UPDATE
 const currentTile = () => {
-    return currentRow().querySelector(':nth-child('+ word.length +')')
-}
+  return currentRow().querySelector(":nth-child(" + (word.length + 1) + ")");
+};
 
-//CURRENT ROW 
+//CURRENT ROW
 const currentRow = () => {
-    return document.querySelector('.row:nth-child('+ tries +')')
-}
+  return document.querySelector(".row:nth-child(" + tries + ")");
+};
 
 //JUDGE RESULT
 const judgeResult = () => {
-    if(word === solution){
-        
-        let audio = new Audio('sound/sound.mp3');
-		audio.play();
-        animateTileDance(currentRow())
-
-    }
-    
-    else if( tries >= maxTries){
-        
-
-        youLose()
-        
-    }
-
-    else{
-        word = ''
-        tries++
-    }
-}
-
-
-
-
+  if (word === solution) {
+    let audio = new Audio("sound/sound.mp3");
+    audio.play();
+    animateTileDance(currentRow());
+  }
+  else if (tries >= maxTries) {
+    youLose();
+  }
+  else {
+    word = "";
+    tries++;
+  }
+};
--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,34 @@
-
 //AUTHENTIFICATION
 
+const isLoggedIn = () => {
+  return window.localStorage.getItem("loggedIn");
+};
+
 const startUp = () => {
-    location.href = 'account.html'
+  if (isLoggedIn()) {
+    if (location.href.endsWith("account.html")) location.href = "index.html";
+  } else {
+    if (!location.href.endsWith("account.html")) location.href = "account.html";
+  }
+};
+
+const login = (event) => {
+  console.log(event);
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email === "admin" && password === "1234") {
+    window.localStorage.setItem("loggedIn", true);
+    startUp();
+  }
+};
+
+if (location.href.endsWith("account.html")) {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      login();
+    }
+  });
 }
 
 startUp();
-
